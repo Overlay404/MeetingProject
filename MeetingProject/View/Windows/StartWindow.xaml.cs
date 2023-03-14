b4503a5b390284755b201a5e243cca20f7ecb12c
Overlay404 <denis.alekseev.200411@gmail.com>
2023-03-14 19:27:52 +0300

    Создал окно авторизации

---
+++ MeetingProject/View/Windows/StartWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MeetingProject.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) { DragMove(); }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e) { ShutdownApplication(); }
+
+        private static void ShutdownApplication()
+        {
+            if (MessageBox.Show("Завершить сеанс?", "Выход", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                Application.Current.Shutdown();
+        }
+    }
+}
