43a04e080dccaa1a6b284efbfcdf22663a6e9849
Overlay404 <denis.alekseev.200411@gmail.com>
2023-04-17 09:29:21 +0300

    доделать

--- MeetingProject/View/Windows/EditingProjectWindow.xaml.cs
+++ MeetingProject/View/Windows/EditingProjectWindow.xaml.cs
@@ -22,20 +22,98 @@ namespace MeetingProject.View.Windows
     /// </summary>
     public partial class EditingProjectWindow : Window
     {
+        public List<HintMarkdownSyntax> Hint
+        {
+            get { return (List<HintMarkdownSyntax>)GetValue(HintProperty); }
+            set { SetValue(HintProperty, value); }
+        }
+
+        public static readonly DependencyProperty HintProperty =
+            DependencyProperty.Register("Hint", typeof(List<HintMarkdownSyntax>), typeof(EditingProjectWindow));
+
+
+        public Visibility VisibilityListBox
+        {
+            get { return (Visibility)GetValue(VisibilityListBoxProperty); }
+            set { SetValue(VisibilityListBoxProperty, value); }
+        }
+
+        public static readonly DependencyProperty VisibilityListBoxProperty =
+            DependencyProperty.Register("VisibilityListBox", typeof(Visibility), typeof(EditingProjectWindow));
+
+
+
         public static EditingProjectWindow Instance;
 
         public EditingProjectWindow(Project project = null)
         {
+            VisibilityListBox = Visibility.Hidden;
+            InitializeHintMarkdownSyntax();
             InitializeComponent();
             FramePageEditing.Navigate(new EditingProjectPage(project));
             Instance = this;
+
 
             SettingBtn.MouseDown += (sender, e) => { FramePageEditing.Navigate(new SettingProjectPage(project)); };
             ExitBtn.MouseDown += (sender, e) =>
             {
                 ClosingWindow(false);
             };
-            HelpBtn.MouseEnter += (sender, e) => {  };
+            HelpBtn.MouseDown += (sender, e) => 
+            {
+                if(VisibilityListBox == Visibility.Hidden)
+                {
+                    VisibilityListBox = Visibility.Visible;
+                }
+                else
+                {
+                    VisibilityListBox = Visibility.Hidden;
+                }
+            };
+            HintListBox.SelectionChanged += (sender, e) =>
+            {
+                EditingProjectPage.
+            };
+        }
+
+        private void InitializeHintMarkdownSyntax()
+        {
+            Hint = new List<HintMarkdownSyntax>
+            {
+                new HintMarkdownSyntax
+                {
+                    Header = "Заголовок 1",
+                    Content = "# Заголовок"
+                },
+
+                new HintMarkdownSyntax
+                {
+                    Header = "Заголовок 2",
+                    Content = "## Заголовок"
+                },
+
+                new HintMarkdownSyntax
+                {
+                    Header = "Заголовок 3",
+                    Content = "### Заголовок"
+                },
+
+                new HintMarkdownSyntax
+                {
+                    Header = "Заголовок 4",
+                    Content = "#### Заголовок"
+                },
+
+                new HintMarkdownSyntax
+                {
+                    Header = "Таблица",
+                    Content = @"
+| Заголовок 1 | Заголовок 2 |
+| ----------- | ----------- |
+| Строка 1, ячейка 1 | Строка 1, ячейка 2 |
+| Строка 2, ячейка 1 | Строка 2, ячейка 2 |"
+                }
+            };
         }
 
         private void ClosingWindow(bool IsClosing)
@@ -52,5 +130,11 @@ namespace MeetingProject.View.Windows
         {
             ClosingWindow(true);
         }
+    }
+
+    public class HintMarkdownSyntax
+    {
+        public string Header { get; set; }
+        public string Content { get; set; }
     }
 }
